cc97c37b5a64e59e989872b5083038a0839663b3
Aya-Nori <atc.btm@gmail.com>
2023-09-06 21:15:04 +0900

    新規登録データの保存設定
    registrations/newに入力した値を保存できるよう、application_controllerで設定した。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
 
   private
@@ -9,4 +10,14 @@ class ApplicationController < ActionController::Base
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]  # 環境変数を読み込む記述に変更
     end
   end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name_sei])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name_mei])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name_sei_kana])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name_mei_kana])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:birth])
+  end
+
 end
