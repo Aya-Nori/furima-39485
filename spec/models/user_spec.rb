18111eb9a41f6a763e9a78432d2359ffaf04e30e
Aya-Nori <atc.btm@gmail.com>
2023-09-08 22:04:19 +0900

    factory_bot,fakerの導入

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  describe 'ユーザー新規登録' do
+    it 'emailが空では登録できない' do
+      @user.email = ''
+      @user.valid?
+      expect(user.errors.full_messages).to include("Email can't be blank")
+    end
+    it 'passwordが空だと登録できない' do
+      @user.password = ''
+      @user.valid?
+      expect(user.errors.full_messages).to include("Password can't be blank")
+    end
+    end
 end
+
