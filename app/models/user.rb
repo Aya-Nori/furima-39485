ab760abc8da29fb39b6bbb214b3ce849a16cc525
Aya-Nori <atc.btm@gmail.com>
2023-09-10 14:51:12 +0900

    passwordのバリデーション変更
    models/user.rb におけるバリデーション変更。deviseでの設定を外し、別途設定した。

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,14 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable
 
   validates :nickname, presence: true
   validates :birth, presence: true
 
+  with_options presence: true, length: { minimum: 6 },  format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: '英字と数字の両方を含めて設定してください' } do
+    validates :password, presence: true
+  end
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
     validates :name_sei, presence: true
     validates :name_mei, presence: true
