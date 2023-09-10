e6793fa86489c130df0fcf2bc9610a0220cb647e
Aya-Nori <atc.btm@gmail.com>
2023-09-10 17:41:33 +0900

    FactoryBotの記述修正
    Fakerの記述を修正した。姓（漢字）とパスワードに英数が必ず入ること

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -6,10 +6,10 @@ FactoryBot.define do
 
     nickname { Faker::Name.initials(number: 8) }
     email { Faker::Internet.email }
-    password { Faker::Internet.password(min_length: 6) }
+    password { Faker::Internet.password(min_length: 6, min_alpha: 1, min_numeric: 1) }
     password_confirmation { password }
     birth { Faker::Date.birthday }
-    name_sei { [person.first.kanji, person.first.hiragana, person.first.katakana].sample }
+    name_sei { [person.last.kanji, person.last.hiragana, person.last.katakana].sample }
     name_mei { [person.first.kanji, person.first.hiragana, person.first.katakana].sample }
     name_sei_kana { person.last.katakana }
     name_mei_kana { person.first.katakana }
