dce48db36cb10d4e0e3037585db8c7a1f4a063ea
Aya-Nori <atc.btm@gmail.com>
2023-09-11 22:00:27 +0900

    カナ入力のバリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User < ApplicationRecord
     validates :name_sei, presence: true
     validates :name_mei, presence: true
   end
-  with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: '全角カナを使用してください' } do
+  with_options presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: '全角カナを使用してください' } do
     validates :name_sei_kana, presence: true
     validates :name_mei_kana, presence: true
   end
