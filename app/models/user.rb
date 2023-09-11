ade1d169fc0a46da7ab9fe966fdb9d3e19b05fbc
Aya-Nori <atc.btm@gmail.com>
2023-09-11 21:32:16 +0900

    models/user.rb　修正
    emailのバリデーションを削除した。

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,6 @@ class User < ApplicationRecord
   validates :nickname, presence: true
   validates :birth, presence: true
 
-  with_options presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i } do
-    validates :email, presence: true, uniqueness: { case_sensitive: false }
-  end
   with_options presence: true, length: { minimum: 6 }, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i } do
     validates :password, presence: true, confirmation: true
   end
