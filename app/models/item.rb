203790a3784104f81c0183cb3c669eb7031d7e72
Aya-Nori <atc.btm@gmail.com>
2023-09-12 21:55:01 +0900

    itemのアクティブハッシュに関するバリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,22 @@ class Item < ApplicationRecord
   has_many :comments
   has_one :purchase
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :charge
+  belongs_to :region
+  belongs_to :estimate
+
+  # 商品出品時に表題(name)がない場合は、保存できない。
+  validates :name, :text, presence: true
+
+  # アクティブハッシュの選択肢は必ず選ぶこと。
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :charge_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :region_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :estimate_id, numericality: { other_than: 1 , message: "can't be blank"} 
+
 end
