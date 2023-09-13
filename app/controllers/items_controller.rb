5970995cc1c18b94f739acacfddb3f1d091746ae
Aya-Nori <atc.btm@gmail.com>
2023-09-13 17:47:29 +0900

    出品の保存実装
    ルーティング、コントローラーを設定した。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,23 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.create(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :image, :description, :price, :category_id, :condition_id, :charge_id, :region_id, :estimate_id).merge(user_id: current_user.id)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
-  resources :items
+  resources :items, only: [:index, :new, :create]
+
   get 'furimas/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
