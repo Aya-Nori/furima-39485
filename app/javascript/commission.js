ba370bb7076224fbc9b1a814168773009f058123
Aya-Nori <atc.btm@gmail.com>
2023-09-14 12:20:21 +0900

    販売手数料や販売利益の表示実装
    JavaScriptで販売手数料や販売利益の表示を実装した

---
+++ app/javascript/commission.js
@@ -0,0 +1,18 @@
+function commission (){
+  console.log("手数料計算機能を実装");
+  const articleText  = document.getElementById("item-price");
+  articleText.addEventListener("keyup", () => {
+    console.log(articleText.value);
+    const priceVal  = articleText.value;
+
+    const commissionVal = document.getElementById("add-tax-price");
+    const calculatedValue = Math.floor(priceVal * 0.1);
+    commissionVal.innerHTML = `${calculatedValue}`;
+
+    const profitVal = document.getElementById("profit");
+    const totalValue = parseInt(priceVal) + parseInt(calculatedValue);
+    profitVal.innerHTML = `${totalValue}`;
+  });
+};
+
+window.addEventListener('turbo:load', commission);
---
+++ app/javascript/index.js
@@ -0,0 +1,5 @@
+function commission (){
+  console.log("手数料計算機能を実装");
+};
+
+window.addEventListener('turbo:load', commission);
