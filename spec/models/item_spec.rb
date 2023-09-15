245e4aa9d22d880d27955fff6b7fa514f58e1f2f
Aya-Nori <atc.btm@gmail.com>
2023-09-15 20:51:25 +0900

    単体テストコード2
    商品の状態について

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -22,11 +22,13 @@ RSpec.describe Item, type: :model do
       end
       it 'カテゴリーが選択されていないときは商品登録できない' do
         @item.category_id = 1
-        # binding.pry
         @item.valid?
         expect(@item.errors.full_messages).to include("Category can't be blank")
       end
       it '商品の状態が選択されていないときは商品登録できない' do
+        @item.condition_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Condition can't be blank")
       end
       it '配送料の負担が選択されていないときは商品登録できない' do
       end
