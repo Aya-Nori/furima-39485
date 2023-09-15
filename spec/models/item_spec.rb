ca90715ed6397a50f0e033ff1f4b8e2c2d2603be
Aya-Nori <atc.btm@gmail.com>
2023-09-15 20:53:25 +0900

    単体テストコード4
    地域について

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -36,6 +36,9 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Charge can't be blank")
       end
       it '配送元の地域が選択されていないときは商品登録できない' do
+        @item.region_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Region can't be blank")
       end
       it '配送までの日数が選択されていないときは商品登録できない' do
       end
