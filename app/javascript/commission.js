33894cb1fa4c9a8fe5409b75844ba7a6a75dcf50
Aya-Nori <atc.btm@gmail.com>
2023-09-16 22:19:16 +0900

    修正2
    console.logの削除

--- app/javascript/commission.js
+++ app/javascript/commission.js
@@ -1,12 +1,10 @@
 function commission (){
-  console.log("手数料の計算機能を実装");
   const articleText = document.getElementById("item-price");
 
   articleText.addEventListener("keyup", () => {
 
     console.log(articleText.value);
     const priceVal  = articleText.value;
-    console.log("経過確認");
     const commissionVal = document.getElementById("add-tax-price");
     const calculatedValue = Math.floor(priceVal * 0.1);
     commissionVal.innerHTML = `${calculatedValue}`;
