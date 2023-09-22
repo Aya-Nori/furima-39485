e1bd009a96dd42e8aea3e8d402e233eb1876824b
Aya-Nori <atc.btm@gmail.com>
2023-09-22 22:56:39 +0900

    マイグレーションファイル修正

--- db/migrate/20230922090258_create_shipments.rb
+++ db/migrate/20230922090258_create_shipments.rb
@@ -1,7 +1,13 @@
 class CreateShipments < ActiveRecord::Migration[7.0]
   def change
     create_table :shipments do |t|
-
+      t.references   :puchase,     null: false, foreign_key: true
+      t.string       :postcode,    null: false
+      t.integer      :region_id,   null: false
+      t.string       :cith,        null: false
+      t.string       :area_number, null: false
+      t.string       :building,    null: false
+      t.string       :tell,        null: false
       t.timestamps
     end
   end
