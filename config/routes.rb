da025e534ef22a4d52d54b1ea17a2ed0fe2ef254
Aya-Nori <atc.btm@gmail.com>
2023-09-24 20:00:39 +0900

    paymentsのルーティング、コントローラーindex設定

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   resources :items, only: [:index, :new, :create, :show, :edit, :update, :destroy] do
     resources :shipments, only: [:index, :new, :create, :show]
   end
+  resources :payments, only: :index
 end
