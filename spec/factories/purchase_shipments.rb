5a9077668b966677ed8f2b31526b1af0dd13130c
Aya-Nori <atc.btm@gmail.com>
2023-09-28 23:07:55 +0900

    Faker調整

--- spec/factories/purchase_shipments.rb
+++ spec/factories/purchase_shipments.rb
@@ -1,10 +1,13 @@
 FactoryBot.define do
   factory :purchase_shipment do
+    address = Gimei.address
+    
     postcode { '123-4567' }
-    region_id { 2 }
-    city { '東京都' }
+    region_id { Faker::Number.between(from: 2, to: 48) }
+    city { address.city.kanji }
     area_number { '1-1' }
     building { '東京ハイツ' }
     tell { '09012341234' }
+
   end
 end
