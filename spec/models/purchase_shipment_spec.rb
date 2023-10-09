e4792712cd61824260e256f8d9d49d09a4bc1d7c
Aya-Nori <atc.btm@gmail.com>
2023-10-09 17:29:00 +0900

    レビュー対応7
    テストコードの修正を実施した

--- spec/models/purchase_shipment_spec.rb
+++ spec/models/purchase_shipment_spec.rb
@@ -2,9 +2,10 @@ require 'rails_helper'
 
 RSpec.describe PurchaseShipment, type: :model do
   describe '購入者情報の保存' do
-    before do
-      user = FactoryBot.create(:user)
-      @purchase_shipment = FactoryBot.build(:purchase_shipment, user_id: user.id)
+    user = FactoryBot.create(:user)
+    item = FactoryBot.create(:item)
+  before do
+      @purchase_shipment = FactoryBot.build(:purchase_shipment, user_id: user.id, item_id: item.id)
     end
 
     context '購入者の記入内容に問題ない場合' do
