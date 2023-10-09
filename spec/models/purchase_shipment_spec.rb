00ff7be23403a607718bfbcb6bd15f65b5981428
Aya-Nori <atc.btm@gmail.com>
2023-10-09 17:31:28 +0900

    Rubocop実施

--- spec/models/purchase_shipment_spec.rb
+++ spec/models/purchase_shipment_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe PurchaseShipment, type: :model do
   describe '購入者情報の保存' do
     user = FactoryBot.create(:user)
     item = FactoryBot.create(:item)
-  before do
+    before do
       @purchase_shipment = FactoryBot.build(:purchase_shipment, user_id: user.id, item_id: item.id)
     end
 
