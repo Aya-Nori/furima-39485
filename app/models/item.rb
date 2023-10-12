e9fbd26c13ba8689907e80fb4e1d52d3e0041b71
Aya-Nori <atc.btm@gmail.com>
2023-10-12 21:11:34 +0900

    重複エラーメッセージの修正
    出品時のバリエーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,7 +12,7 @@ class Item < ApplicationRecord
   belongs_to :estimate
 
   # 商品出品時に商品名、商品の説明、画像、価格がない場合は、保存できない。
-  validates :name, :description, :image, :price, presence: true
+  validates :description, :image, :price, presence: true
   validates :price, numericality: { only_integer: true, in: 300..9_999_999 }
   validates :name, length: { maximum: 40 }
   validates :description, length: { maximum: 1000 }
